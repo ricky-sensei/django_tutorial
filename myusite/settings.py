42b94ca4fb7eaa59aaec6065b95d409d368881d6
ricky-sensei <72785858+ricky-sensei@users.noreply.github.com>
2023-06-30 03:18:43 +0900

    その②モデルまで

--- myusite/settings.py
+++ myusite/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # ここにpollsアプリを追加
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
+
 ]
 
 MIDDLEWARE = [
